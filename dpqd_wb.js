aad6b1652d5816717226a9bea802aeae0f1dbb59
greenwave1987 <48582745+greenwave1987@users.noreply.github.com>
2022-07-28 09:34:31 +0800

    Update dpqd_wb.js

--- dpqd_wb.js
+++ dpqd_wb.js
@@ -191,14 +191,14 @@ function O0QQ(_0xe2613d, _0x4c3135) {
         //执行第二步，为token提供者助力挖宝
             console.log(new Date().Format("hh:mm:ss.S")+'等到00:01开始助力')
             if(new Date().getMinutes()==59){
-                await $.wait((getRandomNumberByRange(120, 200)-new Date().getSeconds())*1000)
+                await $.wait((getRandomNumberByRange(180, 260)-new Date().getSeconds())*1000)
         // 获取API接口数据
                 apidata = await readapi('TOKEN',TK_SIGN.id,TK_SIGN.sign)
         // 获取挖宝助力码
                 fcwb = JSON.parse(apidata.fcwb)
                 await wbzl()
             }else if(new Date().getMinutes()<1){
-                await $.wait((getRandomNumberByRange(50, 100)-new Date().getSeconds())*1000)
+                await $.wait((getRandomNumberByRange(110, 160)-new Date().getSeconds())*1000)
         // 获取API接口数据
                 apidata = await readapi('TOKEN',TK_SIGN.id,TK_SIGN.sign)
         // 获取挖宝助力码
