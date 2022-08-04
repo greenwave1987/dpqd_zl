7f2787f0e02bddf59b5091c6bd43722a213ccb5f
greenwave1987 <48582745+greenwave1987@users.noreply.github.com>
2022-08-04 10:49:13 +0800

    Update dpqd_wb.js

--- dpqd_wb.js
+++ dpqd_wb.js
@@ -304,7 +304,7 @@ function taskUrl(token,venderId,activityId) {
 async function readapi(model_name,id,sign) {
     let datatemp
     await $.wait(id*50)
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 2; i++) {
         try {
             let {data} = await axios.get(`${new Buffer.from('aHR0cDovL2hkMjE1LmFwaS55ZXNhcGkuY24vYXBpL0FwcC9UYWJsZS9HZXQ/YXBwX2tleT0wNkU2MjhGQzIyMzM2NkU2MEIxQTUzRjAxMkMxRTc2OA==', 'base64').toString()}&model_name=${model_name}&id=${id}&sign=${sign}`)
             if (data.ret===200&data.data.err_code===0) {
@@ -326,7 +326,7 @@ async function readapi(model_name,id,sign) {
     return(datatemp)
 }
 async function count(id,field) {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 2; i++) {
         try {
             let {data} = await axios.get(`${new Buffer.from('aHR0cDovL2hkMjE1LmFwaS55ZXNhcGkuY24vPyZzPUFwcC5UYWJsZS5DaGFuZ2VOdW1iZXIuaHRtbCZhcHBfa2V5PTA2RTYyOEZDMjIzMzY2RTYwQjFBNTNGMDEyQzFFNzY4Jm1vZGVsX25hbWU9VE9LRU4mY2hhbmdlX3ZhbHVlPTE=', 'base64').toString()}&id=${id}&change_field=${field}`)
             if (data.ret===200&data.data.err_code===0) {
